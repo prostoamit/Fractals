89a3352da7c35fc1e3853ad982f5d58f6b70b7bb
Prostev_Timofey <prostev_timofey@mail.ru>
2020-11-23 20:47:45 +0300

    Changed class variables names

--- fractal_trees/objects/Branch.py
+++ fractal_trees/objects/Branch.py
@@ -3,7 +3,7 @@ from math import sin, cos, pi
 
 
 class Branch:
-    def __init__(self, screen, color, begin, len, len_step, angle):
+    def __init__(self, screen, color, begin, length, len_step, angle):
 
         # screen - pygame display
         # color - branch color
@@ -13,29 +13,29 @@ class Branch:
         #
         # length - branch length
         #
-        # len_step - changing branch length with every layer
+        # len_step - changing git branch length with every layer
         #
         # angle - angle between branches
 
         self.screen = screen
         self.color = color
 
-        self.b_x, self.b_y = self.begin = begin
+        self.begin_x, self.begin_y = self.begin = begin
 
         self.angle = angle
 
-        self.e_x = self.b_x - int(len * cos(self.angle))
-        self.e_y = self.b_y - int(len * sin(self.angle))
-        self.end = self.e_x, self.e_y
+        self.end_x = self.begin_x - int(length * cos(self.angle))
+        self.end_y = self.begin_y - int(length * sin(self.angle))
+        self.end = self.end_x, self.end_y
 
-        self.len = len
+        self.length = length
         self.len_step = len_step
 
     def draw(self, min_l):
         pygame.draw.line(self.screen, self.color, self.begin, self.end, 3)
-        if self.len - self.len_step >= min_l:
-            branch_l = Branch(self.screen, self.color, self.end, self.len - self.len_step, self.len_step, self.angle + self.len_step/100)
+        if self.length - self.len_step >= min_l:
+            branch_l = Branch(self.screen, self.color, self.end, self.length - self.len_step, self.len_step, self.angle + self.len_step / 100)
             branch_l.draw(min_l)
 
-            branch_r = Branch(self.screen, self.color, self.end, self.len - self.len_step, self.len_step, pi - (self.angle + self.len_step/100))
+            branch_r = Branch(self.screen, self.color, self.end, self.length - self.len_step, self.len_step, pi - (self.angle + self.len_step / 100))
             branch_r.draw(min_l)
