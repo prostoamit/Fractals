cf6c64a2b16a1d2bc568128dfe155c3c25b5fc59
prostev_timofey <prostev_timofey@mail.ru>
2020-12-03 11:16:08 +0300

    Added vector to line

--- objects/line.py
+++ objects/line.py
@@ -6,10 +6,18 @@ class Line:
         self.screen = screen
         self.color = color
 
-        self.begin_position = begin_position
-        self.end_position = end_position
+        self.begin_position = self.b_x, self.b_y = begin_position
+        self.end_position = self.e_x, self.e_y = end_position
+
+        self.vector = pygame.math.Vector2((self.e_x - self.b_x, self.e_y - self.b_y))
 
         self.width = width
+
+    def rotate(self, angle):
+        self.vector.rotate(angle)
+
+    def sclae(self, length):
+        self.vector.scale_to_length(length)
 
     def render(self):
         pygame.draw.line(self.screen, self.color, self.begin_position, self.end_position, self.width)
